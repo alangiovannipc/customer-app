4fc298f70889d3b806a0da08124494e1045fe968
alan.polar <alan.polar@beauthentic.digital>
2019-10-16 18:47:05 -0500

    [feature/customer] create customer module and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CustomerModule } from './customer/customer.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    CustomerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/customer/components/index.ts
@@ -0,0 +1,13 @@
+import {RegisterComponent} from './register/register.component';
+import { ListComponent } from './list/list.component';
+import { ProjectionComponent } from './projection/projection.component';
+
+export const customerComponents: any[] = [
+  RegisterComponent,
+  ListComponent,
+  ProjectionComponent
+];
+
+export * from './register/register.component';
+export * from './list/list.component';
+export * from './projection/projection.component';
